aa0390fdc3c32ef3b28efeebfb032c60faa29235
Otavio <estudosprogotavio@gmail.com>
2026-08-17 03:58:41 -0300

    feat;Adicionando OrderFlowSenderApplication

---
+++ src/main/java/dev/zerphyis/orderflowsender/OrderflowsenderApplication.java
@@ -0,0 +1,13 @@
+package dev.zerphyis.orderflowsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderflowsenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderflowsenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/zerphyis/orderflowsender/OrderflowsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zerphyis.orderflowsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderflowsenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
