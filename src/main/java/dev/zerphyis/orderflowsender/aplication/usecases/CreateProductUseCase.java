9f4fb87cb6841984359d017da6e982bb1c9e334a
Otavio <estudosprogotavio@gmail.com>
2026-08-25 05:02:00 -0300

    TASK08 refactor:corrigindo import do dtos em CreateProductUseCase

--- src/main/java/dev/zerphyis/orderflowsender/aplication/usecases/CreateProductUseCase.java
+++ src/main/java/dev/zerphyis/orderflowsender/aplication/usecases/CreateProductUseCase.java
@@ -1,7 +1,7 @@
 package dev.zerphyis.orderflowsender.aplication.usecases;
 
-import dev.zerphyis.orderflowsender.aplication.dtos.ProductRequestDto;
-import dev.zerphyis.orderflowsender.aplication.dtos.ProductResponseDto;
+import dev.zerphyis.orderflowsender.infra.dtos.ProductRequestDto;
+import dev.zerphyis.orderflowsender.infra.dtos.ProductResponseDto;
 import dev.zerphyis.orderflowsender.domain.entity.Product;
 import dev.zerphyis.orderflowsender.domain.interfaceCases.CreateProductInterfaceCase;
 import dev.zerphyis.orderflowsender.domain.repository.ProductRepository;
