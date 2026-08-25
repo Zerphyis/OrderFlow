288c6ee7af18a685b213897dee55d375b973cadf
Otavio <estudosprogotavio@gmail.com>
2026-08-25 05:02:23 -0300

    TASK08 feat:adicionando RemoveProducUseCase

---
+++ src/main/java/dev/zerphyis/orderflowsender/aplication/usecases/RemoveProducUseCase.java
@@ -0,0 +1,28 @@
+package dev.zerphyis.orderflowsender.aplication.usecases;
+
+import dev.zerphyis.orderflowsender.domain.entity.Product;
+import dev.zerphyis.orderflowsender.domain.interfaceCases.RemoveProductInterfaceCase;
+import dev.zerphyis.orderflowsender.domain.repository.ProductRepository;
+
+import java.util.UUID;
+
+public class RemoveProducUseCase implements RemoveProductInterfaceCase {
+    private final ProductRepository productRepository;
+
+    public RemoveProducUseCase(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void execute(UUID id) {
+
+        Product product = productRepository.findById(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("Product not found")
+                );
+
+        product.deactivate();
+
+        productRepository.save(product);
+    }
+}
