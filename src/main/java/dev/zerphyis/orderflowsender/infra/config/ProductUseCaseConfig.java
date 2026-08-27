e82a6719ae307b5e345e5e275a84846894ba418d
Otavio <estudosprogotavio@gmail.com>
2026-08-27 03:57:05 -0300

    TASK10 feat:adicionando bean de CreateProduct em ProductUseCaseConfig

---
+++ src/main/java/dev/zerphyis/orderflowsender/infra/config/ProductUseCaseConfig.java
@@ -0,0 +1,18 @@
+package dev.zerphyis.orderflowsender.infra.config;
+
+import dev.zerphyis.orderflowsender.aplication.usecases.*;
+import dev.zerphyis.orderflowsender.domain.interfaceCases.*;
+import dev.zerphyis.orderflowsender.domain.repository.ProductRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProductUseCaseConfig {
+
+    @Bean
+    public CreateProductInterfaceCase createProductInterfaceCase(ProductRepository repository){
+        return new CreateProductUseCase(repository);
+    }
+
+
+}
