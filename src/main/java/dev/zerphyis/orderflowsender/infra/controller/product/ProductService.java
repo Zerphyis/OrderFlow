179d9da3ca1bfc033efbc06d63e29168ff3dc74d
Otavio <estudosprogotavio@gmail.com>
2026-08-31 06:58:12 -0300

    TASK11 feat: adicionando novas UseCases em ProductService

--- src/main/java/dev/zerphyis/orderflowsender/infra/controller/product/ProductService.java
+++ src/main/java/dev/zerphyis/orderflowsender/infra/controller/product/ProductService.java
@@ -1,6 +1,6 @@
 package dev.zerphyis.orderflowsender.infra.controller.product;
 
-
+import dev.zerphyis.orderflowsender.aplication.exceptions.SkuNotFoundException;
 import dev.zerphyis.orderflowsender.domain.interfaceCases.*;
 import dev.zerphyis.orderflowsender.infra.dtos.ProductRequestDto;
 import dev.zerphyis.orderflowsender.infra.dtos.ProductResponseDto;
@@ -20,19 +20,19 @@ public class ProductService {
     private final CreateProductInterfaceCase createProductUseCase;
     private final UpdateProductInterfaceCase updateProductUseCase;
     private final RemoveProductInterfaceCase removeProductUseCase;
+    private final FindByProductCategoryInterfaceCase findProductsByCategoryUseCase;
+    private final FindBySkuProductInterfaceCase findProductBySkuUseCase;
+    private final ActiveProducInterfaceCase activateProductUseCase;
     private final ListProductInterfaceCase listProductUseCase;
     private final FindByIdProductInterfaceCase findByIdProductUseCase;
 
-    public ProductService(
-            CreateProductInterfaceCase createProductUseCase,
-            UpdateProductInterfaceCase updateProductUseCase,
-            RemoveProductInterfaceCase removeProductUseCase,
-            ListProductInterfaceCase listProductUseCase,
-            FindByIdProductInterfaceCase findByIdProductUseCase
-    ) {
+    public ProductService(CreateProductInterfaceCase createProductUseCase, UpdateProductInterfaceCase updateProductUseCase, RemoveProductInterfaceCase removeProductUseCase, FindByProductCategoryInterfaceCase findProductsByCategoryUseCase, FindBySkuProductInterfaceCase findProductBySkuUseCase, ActiveProducInterfaceCase activateProductUseCase, ListProductInterfaceCase listProductUseCase, FindByIdProductInterfaceCase findByIdProductUseCase) {
         this.createProductUseCase = createProductUseCase;
         this.updateProductUseCase = updateProductUseCase;
         this.removeProductUseCase = removeProductUseCase;
+        this.findProductsByCategoryUseCase = findProductsByCategoryUseCase;
+        this.findProductBySkuUseCase = findProductBySkuUseCase;
+        this.activateProductUseCase = activateProductUseCase;
         this.listProductUseCase = listProductUseCase;
         this.findByIdProductUseCase = findByIdProductUseCase;
     }
@@ -52,19 +52,30 @@ public class ProductService {
         return listProductUseCase.execute(page, size);
     }
 
-    @Caching(
-            put = { @CachePut(value = "product", key = "#id") },
-            evict = { @CacheEvict(value = "productsList", allEntries = true) }
-    )
+    @Caching(put = {@CachePut(value = "product", key = "#id")}, evict = {@CacheEvict(value = "productsList", allEntries = true)})
     public ProductResponseDto update(UUID id, UpdateProductRequestDto requestDto) {
         return updateProductUseCase.execute(id, requestDto);
     }
 
-    @Caching(evict = {
-            @CacheEvict(value = "product", key = "#id"),
-            @CacheEvict(value = "productsList", allEntries = true)
-    })
+    @Caching(evict = {@CacheEvict(value = "product", key = "#id"), @CacheEvict(value = "productsList", allEntries = true)})
     public void remove(UUID id) {
         removeProductUseCase.execute(id);
     }
+
+
+    @Cacheable(value = "productsByCategory", key = "#category")
+    public List<ProductResponseDto> findByCategory(String category) {
+        return findProductsByCategoryUseCase.execute(category);
+    }
+
+    @Cacheable(value = "productBySku", key = "#sku")
+    public ProductResponseDto findBySku(String sku) {
+        return findProductBySkuUseCase.execute(sku).orElseThrow(() -> new SkuNotFoundException(sku));
+    }
+
+    @Caching(put = {@CachePut(value = "product", key = "#id")}, evict = {@CacheEvict(value = "productsList", allEntries = true), @CacheEvict(value = "productsByCategory", allEntries = true), @CacheEvict(value = "productBySku", allEntries = true)})
+    public ProductResponseDto activate(UUID id) {
+        return activateProductUseCase.execute(id);
+    }
+
 }
