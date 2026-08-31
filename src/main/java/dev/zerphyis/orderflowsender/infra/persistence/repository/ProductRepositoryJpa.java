54f8e6aaab6c92d3096178533e569e5d58075806
Otavio <estudosprogotavio@gmail.com>
2026-08-31 06:58:37 -0300

    TASK11 feat: adicionando findByCategoryAndActiveTrue em ProductRepositoryJpa

--- src/main/java/dev/zerphyis/orderflowsender/infra/persistence/repository/ProductRepositoryJpa.java
+++ src/main/java/dev/zerphyis/orderflowsender/infra/persistence/repository/ProductRepositoryJpa.java
@@ -2,7 +2,6 @@ package dev.zerphyis.orderflowsender.infra.persistence.repository;
 
 import dev.zerphyis.orderflowsender.infra.persistence.entity.ProductJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +10,8 @@ import java.util.UUID;
 public interface ProductRepositoryJpa extends JpaRepository<ProductJpaEntity, UUID> {
     Optional<ProductJpaEntity> findByIdAndActiveTrue(UUID id);
 
-    List<ProductJpaEntity> findAllByActiveTrue(Pageable pageable);
+    Optional<ProductJpaEntity> findBySkuAndActiveTrue(String sku);
+
+    List<ProductJpaEntity> findByCategoryAndActiveTrue(String category);
 }
+
