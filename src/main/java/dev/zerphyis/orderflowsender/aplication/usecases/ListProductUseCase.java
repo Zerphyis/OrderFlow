9e24c4b27607fa5aa3c62b4e034bd249d6f36cdd
Otavio <estudosprogotavio@gmail.com>
2026-08-31 07:00:24 -0300

    TASK11 feat:adicionando exceptions em ListProductUseCase

--- src/main/java/dev/zerphyis/orderflowsender/aplication/usecases/ListProductUseCase.java
+++ src/main/java/dev/zerphyis/orderflowsender/aplication/usecases/ListProductUseCase.java
@@ -1,25 +1,39 @@
 package dev.zerphyis.orderflowsender.aplication.usecases;
 
-import dev.zerphyis.orderflowsender.infra.dtos.ProductResponseDto;
 import dev.zerphyis.orderflowsender.domain.entity.Product;
 import dev.zerphyis.orderflowsender.domain.interfaceCases.ListProductInterfaceCase;
 import dev.zerphyis.orderflowsender.domain.repository.ProductRepository;
+import dev.zerphyis.orderflowsender.infra.dtos.ProductResponseDto;
 
 import java.util.List;
 
 public class ListProductUseCase implements ListProductInterfaceCase {
+
     private final ProductRepository repository;
 
     public ListProductUseCase(ProductRepository repository) {
         this.repository = repository;
     }
 
-
     @Override
-    public List<ProductResponseDto> execute(int page, int size) {
-        int offset = page * size;
-        List<Product> products = repository.findAll(offset, size);
+    public List<ProductResponseDto> execute(
+            int page,
+            int size
+    ) {
+        if (page < 0) {
+            throw new IllegalArgumentException(
+                    "Page cannot be negative"
+            );
+        }
 
+        if (size <= 0) {
+            throw new IllegalArgumentException(
+                    "Size must be greater than zero"
+            );
+        }
+
+        List<Product> products =
+                repository.findAll(page, size);
 
         return products.stream()
                 .map(product -> new ProductResponseDto(
