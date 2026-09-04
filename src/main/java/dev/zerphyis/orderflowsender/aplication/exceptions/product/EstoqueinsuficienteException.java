356efbbe44b522d4ffba7ff7c1324861731473e4
Otavio <estudosprogotavio@gmail.com>
2026-09-04 05:38:37 -0300

    TASK14 refactor:corrigindo pacote de exception EstoqueinsuficienteException

--- src/main/java/dev/zerphyis/orderflowsender/aplication/exceptions/product/EstoqueinsuficienteException.java
+++ src/main/java/dev/zerphyis/orderflowsender/aplication/exceptions/product/EstoqueinsuficienteException.java
@@ -1,4 +1,4 @@
-package dev.zerphyis.orderflowsender.aplication.exceptions;
+package dev.zerphyis.orderflowsender.aplication.exceptions.product;
 
 public class EstoqueinsuficienteException extends RuntimeException {
     public EstoqueinsuficienteException(String message) {
